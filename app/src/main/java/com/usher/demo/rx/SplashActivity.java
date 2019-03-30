3b4fdcb9cf27cd205fe67d55a3e0056cfd1b8e5d
zhangzhihai <13504984520@139.com>
2019-03-30 16:37:23 +0800

    rx demo splash

--- app/src/main/java/com/usher/demo/rx/SplashActivity.java
+++ app/src/main/java/com/usher/demo/rx/SplashActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.jakewharton.rxbinding3.view.RxView;
 import com.usher.demo.R;
@@ -12,17 +13,10 @@ import com.usher.demo.utils.RxUtil;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
-import io.reactivex.ObservableTransformer;
-import io.reactivex.Observer;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.schedulers.Schedulers;
-import io.reactivex.subjects.PublishSubject;
 
 public class SplashActivity extends AppCompatActivity {
-    private TextView mSkipTextView;
 
-    private Disposable mDisposable;
+    private static final int COUNTDOWN_SECONDS = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,127 +26,19 @@ public class SplashActivity extends AppCompatActivity {
         initView();
     }
 
-    private PublishSubject<String> subject = PublishSubject.create();
-    private boolean mSkipFlag = false;
-
     private void initView() {
-        mSkipTextView = findViewById(R.id.skip_textview);
-//        mSkipTextView.setText(getString(R.string.splash_skip, 1));
+        TextView mSkipTextView = findViewById(R.id.skip_textview);
 
-        Disposable d = RxView.clicks(mSkipTextView)
-                .subscribe(o -> {
-                    Log.i("zzh", "Click");
-                    mSkipFlag = true;
-                    subject.onNext("Hello");
-                });
-
-        findViewById(R.id.dispose_button).setOnClickListener(v -> {
-            if (mDisposable != null && !mDisposable.isDisposed()) {
-                mDisposable.dispose();
-            }
-        });
-        Observable<Integer> click$ = subject.map(v -> 1).scan((x, y) -> x + y).map(r -> r % 2); //1010
-//        Observable<Integer> startCondition = click$.filter(x -> x == 1);
-
-        Observable<Long> countdown$ = Observable.interval(0, 1, TimeUnit.SECONDS)
-                .map(v -> 5 - v)
-                .takeUntil(click$.filter(x -> x == 0).doOnNext(v -> Log.i("zzh", "In: " + v)));
-
-//        countdown$.subscribe(new Observer<Long>() {
-//            @Override
-//            public void onSubscribe(Disposable d) {
-//                mDisposable = d;
-//            }
-//
-//            @Override
-//            public void onNext(Long aLong) {
-//                Log.i("zzh", "[Countdown] onNext: " + aLong);
-//                mSkipTextView.setText(getString(R.string.splash_skip, String.valueOf(aLong)));
-//            }
-//
-//            @Override
-//            public void onError(Throwable e) {
-//                Log.i("zzh", "[Countdown] onError");
-//            }
-//
-//            @Override
-//            public void onComplete() {
-//                Log.i("zzh", "[Countdown] onComplete");
-//            }
-//        });
-
-        click$.filter(x -> x == 1)
-                .doOnNext(v -> Log.i("zzh", "Out: " + v))
-                .flatMap(a -> Observable.interval(0, 1, TimeUnit.SECONDS)
-                        .map(v -> 5 - v)
-                        .takeUntil(click$.filter(x -> x == 0).doOnNext(v -> Log.i("zzh", "In: " + v)))
-                )
-                .subscribe(new Observer<Long>() {
-                    @Override
-                    public void onSubscribe(Disposable d) {
-                        mDisposable = d;
-                    }
-
-                    @Override
-                    public void onNext(Long aLong) {
-                        Log.i("zzh", "[Countdown] onNext: " + aLong);
-                        mSkipTextView.setText(getString(R.string.splash_skip, String.valueOf(aLong)));
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        Log.i("zzh", "[Countdown] onError");
-                    }
-
-                    @Override
-                    public void onComplete() {
-                        Log.i("zzh", "[Countdown] onComplete");
-                    }
-                });
-
-        startCountdown();
-    }
-
-    <T> ObservableTransformer<T, T> getThreadTransformer() {
-        return upstream -> upstream
-                .subscribeOn(Schedulers.io())
-                .unsubscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread());
-    }
-
-
-    private void startCountdown() {
-        Observable.interval(0, 1, TimeUnit.SECONDS)
-                .map(v -> 3 - v)
-                .takeWhile(v -> v >= 0)
-                .compose(getThreadTransformer())
-                .subscribe(new Observer<Long>() {
-                    @Override
-                    public void onSubscribe(Disposable d) {
-                        mDisposable = d;
-                    }
-
-                    @Override
-                    public void onNext(Long aLong) {
-                        Log.i("zzh", "[Countdown] onNext: " + aLong);
-                        mSkipTextView.setText(getString(R.string.splash_skip, String.valueOf(aLong)));
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        Log.i("zzh", "[Countdown] onError");
-                    }
-
-                    @Override
-                    public void onComplete() {
-                        Log.i("zzh", "[Countdown] onComplete");
-                    }
-                });
-
-        Observable.interval(1000, TimeUnit.SECONDS)
-                .take(10)
+        Observable.interval(0, 1000, TimeUnit.MILLISECONDS)
+                .map(v -> COUNTDOWN_SECONDS - v)
+                .take(COUNTDOWN_SECONDS + 1)
+                .compose(RxUtil.getSchedulerComposer())
+                .takeUntil(RxView.clicks(mSkipTextView).take(1))
+                .doOnComplete(() -> {
+                    Toast.makeText(this, "Countdown Done", Toast.LENGTH_SHORT).show();
+                    mSkipTextView.setEnabled(false);
+                })
                 .as(RxUtil.autoDispose(this))
-                .subscribe(v -> {
-                });
+                .subscribe(v -> mSkipTextView.setText(getString(R.string.splash_skip, String.valueOf(v))));
     }
 }
