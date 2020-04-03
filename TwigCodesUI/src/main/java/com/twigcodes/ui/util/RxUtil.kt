a2b994a71699699e406aeedc1e66da70faf475ab
zhangzhihai <13504984520@139.com>
2020-04-03 15:50:42 +0800

    koltin rx util

---
+++ TwigCodesUI/src/main/java/com/twigcodes/ui/util/RxUtil.kt
@@ -0,0 +1,26 @@
+package com.twigcodes.ui.util
+
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import com.uber.autodispose.AutoDispose
+import com.uber.autodispose.AutoDisposeConverter
+import com.uber.autodispose.android.lifecycle.AndroidLifecycleScopeProvider
+import io.reactivex.ObservableTransformer
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
+
+object RxUtil {
+    @JvmStatic
+    fun <T> autoDispose(owner: LifecycleOwner): AutoDisposeConverter<T> = AutoDispose.autoDisposable(AndroidLifecycleScopeProvider.from(owner, Lifecycle.Event.ON_DESTROY))
+
+    @JvmStatic
+    fun <T> getSchedulerComposer(): ObservableTransformer<T, T> = ObservableTransformer { upstream ->
+        upstream
+                .subscribeOn(Schedulers.io())
+                .unsubscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+    }
+
+    fun <T> singleClick(): ObservableTransformer<T, T> = ObservableTransformer { upstream -> upstream.throttleFirst(500, TimeUnit.MILLISECONDS) }
+}
