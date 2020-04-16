1644bbc192f40bd96a570a65aad933d80c3ae4e3
zhangzhihai <13504984520@139.com>
2020-04-16 10:53:30 +0800

    marquee textview

--- app/src/main/java/com/usher/demo/view/MarqueeTextActivity.kt
+++ app/src/main/java/com/usher/demo/view/MarqueeTextActivity.kt
@@ -3,10 +3,17 @@ package com.usher.demo.view
 import android.os.Bundle
 import com.usher.demo.R
 import com.usher.demo.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_marquee_text.*
 
 class MarqueeTextActivity : BaseActivity(Theme.LIGHT) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_marquee_text)
+        initView()
+    }
+
+    private fun initView() {
+        val content = "Centers the image in the display and keeps its aspect ratio. Image is scaled up or down to fit its shorter side to the display. The longer side of the image is cropped."
+        marquee_textview.text = content
     }
 }
