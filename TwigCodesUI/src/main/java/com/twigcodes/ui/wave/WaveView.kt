23dc41b6c256d4be640ff65e864c6276eec71812
zhangzhihai <13504984520@139.com>
2020-04-17 15:39:45 +0800

    wave view

--- TwigCodesUI/src/main/java/com/twigcodes/ui/wave/WaveView.kt
+++ TwigCodesUI/src/main/java/com/twigcodes/ui/wave/WaveView.kt
@@ -6,6 +6,8 @@ import android.util.AttributeSet
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import androidx.core.view.setPadding
+import androidx.core.view.updateLayoutParams
 import androidx.lifecycle.LifecycleOwner
 import com.jakewharton.rxbinding3.view.globalLayouts
 import com.twigcodes.ui.R
@@ -28,14 +30,22 @@ class WaveView @JvmOverloads constructor(context: Context, attrs: AttributeSet? 
 
     private val mFrontWaveColor: Int
     private val mBackWaveColor: Int
-    private var mProgress: Int
     private val mWaveHeight: Int
     private val mWaveLengthMultiple: Float
     private val mWaveHz: Float
+    private var _progress: Int
     private lateinit var mWave: Wave
+
+    var progress: Int
+        get() = _progress
+        set(value) {
+            _progress = min(value, 100)
+            mWave.updateLayoutParams<MarginLayoutParams> { topMargin = (getHeight() * (1 - _progress / 100f)).toInt() }
+        }
 
     init {
         orientation = VERTICAL
+        setPadding(0)
 
         val a = context.theme.obtainStyledAttributes(attrs, R.styleable.WaveView, defStyleAttr, defStyleRes)
         mFrontWaveColor = a.getColor(R.styleable.WaveView_frontColor, DEFAULT_FRONT_WAVE_COLOR)
@@ -43,7 +53,7 @@ class WaveView @JvmOverloads constructor(context: Context, attrs: AttributeSet? 
         mWaveHeight = a.getDimensionPixelSize(R.styleable.WaveView_wave_height, DEFAULT_WAVE_HEIGHT)
         mWaveLengthMultiple = a.getFloat(R.styleable.WaveView_wave_length_multiple, DEFAULT_WAVE_LENGTH_MULTIPLE)
         mWaveHz = a.getFloat(R.styleable.WaveView_wave_hz, DEFAULT_WAVE_HZ)
-        mProgress = a.getInt(R.styleable.WaveView_progress, DEFAULT_PROGRESS)
+        _progress = a.getInt(R.styleable.WaveView_progress, DEFAULT_PROGRESS)
         a.recycle()
 
         initView()
@@ -73,25 +83,13 @@ class WaveView @JvmOverloads constructor(context: Context, attrs: AttributeSet? 
                 .take(1)
                 .`as`(RxUtil.autoDispose(context as LifecycleOwner))
                 .subscribe {
-                    setProgress(mProgress)
+                    progress = _progress
                 }
     }
 
-    fun setProgress(progress: Int) {
-        mProgress = min(progress, 100)
+    fun startWave() = mWave.startWave()
 
-        mWave.layoutParams = (mWave.layoutParams as MarginLayoutParams).apply {
-            topMargin = (getHeight() * (1 - mProgress / 100f)).toInt()
-        }
-    }
-
-    fun startWave() {
-        mWave.startWave()
-    }
-
-    fun stopWave() {
-        mWave.stopWave()
-    }
+    fun stopWave() = mWave.stopWave()
 
     /**
      * y=Asin(ωx+φ)+k
@@ -178,10 +176,11 @@ class WaveView @JvmOverloads constructor(context: Context, attrs: AttributeSet? 
         }
 
         /**
-         * 1. reset
-         * 2. 以左下角为起点
-         * 3. x的取值范围为[0, width), 以[X_INTERVAL]作为取点的间隔, 逐个进行打点
-         * 4. x+[X_INTERVAL]的值可能为>=width, 因此最后再打一个x为width的点
+         * 1. 清空
+         * 2. 以左下角为起点开始打点
+         * 3. x的取值范围为[0, width), 以[X_INTERVAL]作为取点的间隔, 逐个进行打点形成sin曲线
+         * 4. x+[X_INTERVAL]的值可能为>=width, 因此再打一个x为width的点作为sin曲线的结点
+         * 5. 以右下角为终点打最后一个点
          */
         private fun updatePath(path: Path, phi: Double) {
             path.reset()
--- app/src/main/java/com/usher/demo/view/WaveActivity.kt
+++ app/src/main/java/com/usher/demo/view/WaveActivity.kt
@@ -1,10 +1,12 @@
 package com.usher.demo.view
 
 import android.os.Bundle
-import com.twigcodes.ui.wave.WaveView
+import com.twigcodes.ui.util.RxUtil
 import com.usher.demo.R
 import com.usher.demo.base.BaseActivity
+import io.reactivex.Observable
 import kotlinx.android.synthetic.main.activity_wave.*
+import java.util.concurrent.TimeUnit
 
 class WaveActivity : BaseActivity(Theme.LIGHT) {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +16,15 @@ class WaveActivity : BaseActivity(Theme.LIGHT) {
     }
 
     private fun initView() {
-        wave_view.setProgress(100)
+        Observable.interval(200, TimeUnit.MILLISECONDS)
+                .take(100 + 1)
+                .startWith(0)
+                .map { it.toInt() }
+                .compose(RxUtil.getSchedulerComposer())
+                .`as`(RxUtil.autoDispose(this))
+                .subscribe {
+                    wave_view.progress = it
+                    progressbar.progress = it
+                }
     }
 }
