59b21f5b89a738ab662e2d9ab1ab2453d81e3256
zhangzhihai <13504984520@139.com>
2020-04-20 12:45:56 +0800

    wave view

--- TwigCodesUI/src/main/java/com/twigcodes/ui/wave/WaveView.kt
+++ TwigCodesUI/src/main/java/com/twigcodes/ui/wave/WaveView.kt
@@ -22,17 +22,36 @@ class WaveView @JvmOverloads constructor(context: Context, attrs: AttributeSet? 
     companion object {
         private const val DEFAULT_FRONT_WAVE_COLOR = Color.WHITE
         private const val DEFAULT_BACK_WAVE_COLOR = Color.WHITE
-        private const val DEFAULT_WAVE_HEIGHT = 20
         private const val DEFAULT_PROGRESS = 100
+        private const val DEFAULT_WAVE_HEIGHT = 20
         private const val DEFAULT_WAVE_LENGTH_MULTIPLE = 1.0f
-        private const val DEFAULT_WAVE_HZ = 0.09f
+        private const val DEFAULT_WAVE_DURATION = 2000
     }
 
     private val mFrontWaveColor: Int
     private val mBackWaveColor: Int
+
+    /**
+     * 对于sinx来说, 它的取值范围为[-1,1], 对于Asinx来说, 它的取值范围为[-A,A], 这个A就是wave的高度, 那么[Wave]的高度为2A.
+     */
     private val mWaveHeight: Int
+
+    /**
+     * 对于sinx来说, 一个周期的长度为2π, 对于sin0.5x来说, 一个周期的长度为4π, 那么对于sinωx来说, 一个周期的长度为2π/ω
+     * 但是, 如果让用户来对ω进行赋值会是个比较痛疼的事情, 因为它不是一个很直观的长度或系数, 他们更关心的应该是一个周期的长度是多少,
+     * 或者干脆一点, [WaveView]的宽度内显示几个周期, 比如说我想要两个周期的效果, 那么这个参数就传2.
+     *
+     * ω = 2π/waveLength = 2π/(width*multiple)
+     */
     private val mWaveLengthMultiple: Float
-    private val mWaveHz: Float
+
+    /**
+     * sin(x+φ)中φ的取值范围为[0,2π), 如果我们动态调整φ的值, 那么每个点的值便可以在[-1,1]之间变化, 这样整个曲线便可产生类似波浪的动态效果.
+     * 这个参数的目的就是让用户设置波浪上上下下这样一个来回的时间是多少.
+     *
+     * Δφ*period = 2π
+     */
+    private val mWaveDuration: Int
     private var _progress: Int
     private lateinit var mWave: Wave
 
@@ -52,7 +71,7 @@ class WaveView @JvmOverloads constructor(context: Context, attrs: AttributeSet? 
         mBackWaveColor = a.getColor(R.styleable.WaveView_backColor, DEFAULT_BACK_WAVE_COLOR)
         mWaveHeight = a.getDimensionPixelSize(R.styleable.WaveView_wave_height, DEFAULT_WAVE_HEIGHT)
         mWaveLengthMultiple = a.getFloat(R.styleable.WaveView_wave_length_multiple, DEFAULT_WAVE_LENGTH_MULTIPLE)
-        mWaveHz = a.getFloat(R.styleable.WaveView_wave_hz, DEFAULT_WAVE_HZ)
+        mWaveDuration = a.getInt(R.styleable.WaveView_wave_duration, DEFAULT_WAVE_DURATION)
         _progress = a.getInt(R.styleable.WaveView_progress, DEFAULT_PROGRESS)
         a.recycle()
 
@@ -70,7 +89,7 @@ class WaveView @JvmOverloads constructor(context: Context, attrs: AttributeSet? 
             style = Paint.Style.FILL
         }
 
-        mWave = Wave(context).apply { config(mWaveLengthMultiple, mWaveHeight, mWaveHz, frontWavePaint, backWavePaint) }
+        mWave = Wave(context).apply { config(mWaveLengthMultiple, mWaveHeight, mWaveDuration, frontWavePaint, backWavePaint) }
 
         val foundation = Foundation(context).apply { config(frontWavePaint, backWavePaint) }
         val waveParams = LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, mWaveHeight * 2)
@@ -93,11 +112,17 @@ class WaveView @JvmOverloads constructor(context: Context, attrs: AttributeSet? 
 
     /**
      * y=Asin(ωx+φ)+k
+     *
+     * A=waveHeight
+     * ω=2π/(width*multiple)
+     * φ+=Δφ  1. φ∈[0,2π) 2. φ*period=2π
+     * k=waveHeight
      */
     private class Wave(context: Context) : View(context, null, 0) {
         companion object {
             private const val X_INTERVAL = 20f
             private const val PI2 = 2 * Math.PI
+            private const val DELTA_PHI_PERIOD = 25L
         }
 
         private val mStopSubject = PublishSubject.create<Unit>()
@@ -105,10 +130,9 @@ class WaveView @JvmOverloads constructor(context: Context, attrs: AttributeSet? 
         private val mBackWavePath = Path()
         private var mFrontWavePaint = Paint()
         private var mBackWavePaint = Paint()
-        private var mWaveMultiple = 0f
-        private var mWaveLength = 0f
+        private var mWaveLengthMultiple = 0f
         private var mWaveHeight = 0
-        private var mWaveHz = 0f
+        private var mWaveDeltaPhi = 0.0
 
         private val mLeft = 0f
         private var mRight = 0f
@@ -128,25 +152,24 @@ class WaveView @JvmOverloads constructor(context: Context, attrs: AttributeSet? 
                     .take(1)
                     .`as`(RxUtil.autoDispose(context as LifecycleOwner))
                     .subscribe {
-                        mWaveLength = width.toFloat() * mWaveMultiple
                         mRight = width.toFloat()
                         mBottom = height.toFloat()
-                        omega = PI2 / mWaveLength
+                        omega = PI2 / (width.toFloat() * mWaveLengthMultiple)
 
                         startWave()
                     }
         }
 
-        fun config(waveMultiple: Float, waveHeight: Int, waveHz: Float, frontPaint: Paint, backPaint: Paint) {
-            mWaveMultiple = waveMultiple
+        fun config(waveMultiple: Float, waveHeight: Int, waveDuration: Int, frontPaint: Paint, backPaint: Paint) {
+            mWaveLengthMultiple = waveMultiple
             mWaveHeight = waveHeight
-            mWaveHz = waveHz
+            mWaveDeltaPhi = PI2 / (waveDuration.toDouble() / DELTA_PHI_PERIOD)
             mFrontWavePaint = frontPaint
             mBackWavePaint = backPaint
         }
 
         fun startWave() {
-            Observable.interval(25, TimeUnit.MILLISECONDS)
+            Observable.interval(DELTA_PHI_PERIOD, TimeUnit.MILLISECONDS)
                     .takeUntil(mStopSubject)
                     .compose(RxUtil.getSchedulerComposer())
                     .`as`(RxUtil.autoDispose(context as LifecycleOwner))
@@ -166,11 +189,11 @@ class WaveView @JvmOverloads constructor(context: Context, attrs: AttributeSet? 
          * φ取值范围为[0, 2π)
          */
         private fun updatePhi() {
-            mFrontPhi += mWaveHz
+            mFrontPhi += mWaveDeltaPhi
             if (mFrontPhi >= PI2)
                 mFrontPhi -= PI2
 
-            mBackPhi += mWaveHz
+            mBackPhi += mWaveDeltaPhi
             if (mBackPhi >= PI2)
                 mBackPhi -= PI2
         }
