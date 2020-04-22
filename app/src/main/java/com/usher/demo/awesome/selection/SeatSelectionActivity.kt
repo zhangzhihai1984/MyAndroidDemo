3635d79d28e2928fc9463e664782fae8631bd6e9
zhangzhihai <13504984520@139.com>
2020-04-22 13:26:05 +0800

    seat selection

--- app/src/main/java/com/usher/demo/awesome/selection/SeatSelectionActivity.kt
+++ app/src/main/java/com/usher/demo/awesome/selection/SeatSelectionActivity.kt
@@ -13,6 +13,7 @@ import com.jakewharton.rxbinding3.view.globalLayouts
 import com.twigcodes.ui.adapter.RxBaseQuickAdapter
 import com.twigcodes.ui.util.RxUtil
 import com.usher.demo.R
+import io.reactivex.subjects.PublishSubject
 import kotlinx.android.synthetic.main.activity_seat_selection.*
 
 class SeatSelectionActivity : AppCompatActivity() {
@@ -25,6 +26,8 @@ class SeatSelectionActivity : AppCompatActivity() {
 
     data class Seat(var status: Status, var spanSize: Int = 1)
 
+    data class SeatClick(var rowPosition: Int, var columnPosition: Int)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_seat_selection)
@@ -32,62 +35,41 @@ class SeatSelectionActivity : AppCompatActivity() {
     }
 
     private fun initView() {
-//        mFrontAdapter = FrontAdapter(mFrontList)
-//        mFrontAdapter.itemClicks()
-//                .compose(RxUtil.getSchedulerComposer())
-//                .`as`(RxUtil.autoDispose(this))
-//                .subscribe { position ->
-//                    mFrontList[position] = when (mFrontList[position]) {
-//                        Status.IDLE -> Status.SELECTED
-//                        Status.SELECTED -> Status.IDLE
-//                        else -> Status.DISABLED
-//                    }
-//
-//                    updatePresentationList()
-//                }
-
-        recyclerview.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
-        recyclerview.adapter = RowAdapter(listOf(arrayListOf(
+        val seatStatusLists = listOf(arrayListOf(
                 Status.SELECTED, Status.SELECTED,
                 Status.DISABLED,
                 Status.IDLE, Status.IDLE, Status.IDLE,
-                Status.DISABLED, Status.DISABLED)))
+                Status.DISABLED, Status.DISABLED))
+
+        val rowAdapter = RowAdapter(seatStatusLists)
+
+        rowAdapter.seatClicks()
+                .compose(RxUtil.getSchedulerComposer())
+                .`as`(RxUtil.autoDispose(this))
+                .subscribe { click ->
+                    seatStatusLists[click.rowPosition][click.columnPosition] = when (seatStatusLists[click.rowPosition][click.columnPosition]) {
+                        Status.IDLE -> Status.SELECTED
+                        Status.SELECTED -> Status.IDLE
+                        else -> Status.DISABLED
+                    }
+                    rowAdapter.notifyDataSetChanged()
+                }
+
+        recyclerview.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        recyclerview.adapter = rowAdapter
     }
 
-    private class RowViewHolder(view: View) : BaseViewHolder(view) {
+    private class RowAdapter(data: List<List<Status>>) : RxBaseQuickAdapter<List<Status>, RowAdapter.RowViewHolder>(R.layout.item_seat_selection_row, data) {
         companion object {
             private const val SEAT_COUNT_PER_ROW = 8
         }
 
-        val seatAdapter = SeatAdapter(listOf())
-        val spanAdapter = SpanAdapter(listOf())
+        private val mClickSubject = PublishSubject.create<SeatClick>()
 
-        init {
-            view.findViewById<RecyclerView>(R.id.seat_recyclerview).run {
-                layoutManager = GridLayoutManager(context, SEAT_COUNT_PER_ROW, RecyclerView.VERTICAL, false)
-                adapter = seatAdapter
-                globalLayouts()
-                        .take(1)
-                        .`as`(RxUtil.autoDispose(context as LifecycleOwner))
-                        .subscribe { updateLayoutParams { height = getWidth() / SEAT_COUNT_PER_ROW - context.resources.getDimensionPixelSize(R.dimen.selection_item_margin) * 2 } }
-            }
-
-            view.findViewById<RecyclerView>(R.id.span_recyclerview).run {
-                layoutManager = GridLayoutManager(context, SEAT_COUNT_PER_ROW, RecyclerView.VERTICAL, false)
-                adapter = spanAdapter
-                globalLayouts()
-                        .take(1)
-                        .`as`(RxUtil.autoDispose(context as LifecycleOwner))
-                        .subscribe { updateLayoutParams { height = getWidth() / SEAT_COUNT_PER_ROW - context.resources.getDimensionPixelSize(R.dimen.selection_item_margin) * 2 } }
-            }
-        }
-    }
-
-    private class RowAdapter(data: List<List<Status>>) : RxBaseQuickAdapter<List<Status>, RowViewHolder>(R.layout.item_seat_selection_row, data) {
         override fun convert(helper: RowViewHolder, statusList: List<Status>) {
+            helper.itemView.tag = helper.layoutPosition
             helper.seatAdapter.setNewData(statusList)
             helper.spanAdapter.setNewData(getSpanList(statusList))
-
         }
 
         private fun getSpanList(statusList: List<Status>): List<Seat> {
@@ -117,6 +99,40 @@ class SeatSelectionActivity : AppCompatActivity() {
             }
 
             return seatList
+        }
+
+        fun seatClicks() = mClickSubject
+
+        private inner class RowViewHolder(view: View) : BaseViewHolder(view) {
+            val seatAdapter = SeatAdapter(listOf())
+            val spanAdapter = SpanAdapter(listOf())
+
+            init {
+                view.findViewById<RecyclerView>(R.id.seat_recyclerview).run {
+                    layoutManager = GridLayoutManager(mContext, SEAT_COUNT_PER_ROW, RecyclerView.VERTICAL, false)
+                    adapter = seatAdapter
+                    globalLayouts()
+                            .take(1)
+                            .`as`(RxUtil.autoDispose(mContext as LifecycleOwner))
+                            .subscribe { updateLayoutParams { height = getWidth() / SEAT_COUNT_PER_ROW - mContext.resources.getDimensionPixelSize(R.dimen.selection_item_margin) * 2 } }
+                }
+
+                view.findViewById<RecyclerView>(R.id.span_recyclerview).run {
+                    layoutManager = GridLayoutManager(mContext, SEAT_COUNT_PER_ROW, RecyclerView.VERTICAL, false)
+                    adapter = spanAdapter
+                    globalLayouts()
+                            .take(1)
+                            .`as`(RxUtil.autoDispose(mContext as LifecycleOwner))
+                            .subscribe { updateLayoutParams { height = getWidth() / SEAT_COUNT_PER_ROW - mContext.resources.getDimensionPixelSize(R.dimen.selection_item_margin) * 2 } }
+                }
+
+                seatAdapter.itemClicks()
+                        .compose(RxUtil.getSchedulerComposer())
+                        .`as`(RxUtil.autoDispose(mContext as LifecycleOwner))
+                        .subscribe { position ->
+                            mClickSubject.onNext(SeatClick(view.tag as Int, position))
+                        }
+            }
         }
     }
 
