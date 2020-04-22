e5e26ac48026cb8943e9872b71b2f24f84de79b1
zhangzhihai <13504984520@139.com>
2020-04-22 12:48:02 +0800

    seat selection

--- app/src/main/java/com/usher/demo/awesome/selection/SeatSelectionActivity.kt
+++ app/src/main/java/com/usher/demo/awesome/selection/SeatSelectionActivity.kt
@@ -1,9 +1,12 @@
 package com.usher.demo.awesome.selection
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.updateLayoutParams
+import androidx.lifecycle.LifecycleOwner
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.chad.library.adapter.base.BaseViewHolder
 import com.jakewharton.rxbinding3.view.globalLayouts
@@ -13,19 +16,6 @@ import com.usher.demo.R
 import kotlinx.android.synthetic.main.activity_seat_selection.*
 
 class SeatSelectionActivity : AppCompatActivity() {
-    companion object {
-        private const val SEAT_COUNT = 8
-    }
-
-    private val mFrontList = arrayListOf(
-            Status.SELECTED, Status.SELECTED,
-            Status.DISABLED,
-            Status.IDLE, Status.IDLE, Status.IDLE,
-            Status.DISABLED, Status.DISABLED
-    )
-    private val mBehindList = arrayListOf<Seat>()
-    private lateinit var mFrontAdapter: FrontAdapter
-    private lateinit var mBehindAdapter: BehindAdapter
 
     enum class Status {
         IDLE,
@@ -42,53 +32,105 @@ class SeatSelectionActivity : AppCompatActivity() {
     }
 
     private fun initView() {
-        mFrontAdapter = FrontAdapter(mFrontList)
-        mFrontAdapter.itemClicks()
-                .compose(RxUtil.getSchedulerComposer())
-                .`as`(RxUtil.autoDispose(this))
-                .subscribe { position ->
-                    mFrontList[position] = when (mFrontList[position]) {
-                        Status.IDLE -> Status.SELECTED
-                        Status.SELECTED -> Status.IDLE
-                        else -> Status.DISABLED
-                    }
+//        mFrontAdapter = FrontAdapter(mFrontList)
+//        mFrontAdapter.itemClicks()
+//                .compose(RxUtil.getSchedulerComposer())
+//                .`as`(RxUtil.autoDispose(this))
+//                .subscribe { position ->
+//                    mFrontList[position] = when (mFrontList[position]) {
+//                        Status.IDLE -> Status.SELECTED
+//                        Status.SELECTED -> Status.IDLE
+//                        else -> Status.DISABLED
+//                    }
+//
+//                    updatePresentationList()
+//                }
 
-                    updatePresentationList()
-                }
-
-
-        seat_recyclerview.layoutManager = GridLayoutManager(this, SEAT_COUNT, RecyclerView.VERTICAL, false)
-        seat_recyclerview.adapter = mFrontAdapter
-        seat_recyclerview.globalLayouts()
-                .take(1)
-                .`as`(RxUtil.autoDispose(this))
-                .subscribe {
-                    seat_recyclerview.updateLayoutParams { height = seat_recyclerview.width / SEAT_COUNT - resources.getDimensionPixelSize(R.dimen.selection_item_margin) * 2 }
-                }
-
-        mBehindAdapter = BehindAdapter(mBehindList)
-        mBehindAdapter.setSpanSizeLookup { _, i -> mBehindList[i].spanSize }
-
-
-        span_recyclerview.layoutManager = GridLayoutManager(this, SEAT_COUNT, RecyclerView.VERTICAL, false)
-        span_recyclerview.adapter = mBehindAdapter
-        span_recyclerview.globalLayouts()
-                .take(1)
-                .`as`(RxUtil.autoDispose(this))
-                .subscribe {
-                    span_recyclerview.updateLayoutParams { height = span_recyclerview.width / SEAT_COUNT - resources.getDimensionPixelSize(R.dimen.selection_item_margin) * 2 }
-                }
-
-        updatePresentationList()
+        recyclerview.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        recyclerview.adapter = RowAdapter(listOf(arrayListOf(
+                Status.SELECTED, Status.SELECTED,
+                Status.DISABLED,
+                Status.IDLE, Status.IDLE, Status.IDLE,
+                Status.DISABLED, Status.DISABLED)))
     }
 
-    private class FrontAdapter(data: List<Status>) : RxBaseQuickAdapter<Status, BaseViewHolder>(R.layout.item_seat_selection_icon, data) {
+    private class RowViewHolder(view: View) : BaseViewHolder(view) {
+        companion object {
+            private const val SEAT_COUNT_PER_ROW = 8
+        }
+
+        val seatAdapter = SeatAdapter(listOf())
+        val spanAdapter = SpanAdapter(listOf())
+
+        init {
+            view.findViewById<RecyclerView>(R.id.seat_recyclerview).run {
+                layoutManager = GridLayoutManager(context, SEAT_COUNT_PER_ROW, RecyclerView.VERTICAL, false)
+                adapter = seatAdapter
+                globalLayouts()
+                        .take(1)
+                        .`as`(RxUtil.autoDispose(context as LifecycleOwner))
+                        .subscribe { updateLayoutParams { height = getWidth() / SEAT_COUNT_PER_ROW - context.resources.getDimensionPixelSize(R.dimen.selection_item_margin) * 2 } }
+            }
+
+            view.findViewById<RecyclerView>(R.id.span_recyclerview).run {
+                layoutManager = GridLayoutManager(context, SEAT_COUNT_PER_ROW, RecyclerView.VERTICAL, false)
+                adapter = spanAdapter
+                globalLayouts()
+                        .take(1)
+                        .`as`(RxUtil.autoDispose(context as LifecycleOwner))
+                        .subscribe { updateLayoutParams { height = getWidth() / SEAT_COUNT_PER_ROW - context.resources.getDimensionPixelSize(R.dimen.selection_item_margin) * 2 } }
+            }
+        }
+    }
+
+    private class RowAdapter(data: List<List<Status>>) : RxBaseQuickAdapter<List<Status>, RowViewHolder>(R.layout.item_seat_selection_row, data) {
+        override fun convert(helper: RowViewHolder, statusList: List<Status>) {
+            helper.seatAdapter.setNewData(statusList)
+            helper.spanAdapter.setNewData(getSpanList(statusList))
+
+        }
+
+        private fun getSpanList(statusList: List<Status>): List<Seat> {
+            val seatList = arrayListOf<Seat>()
+
+            var spanSize: Int
+            var j: Int
+            var i = 0
+            while (i < statusList.size) {
+                spanSize = 1
+                val status = statusList[i]
+                j = i + 1
+                while (j < statusList.size) {
+                    if (statusList[j] == status) {
+                        spanSize += 1
+                    } else {
+                        seatList.add(Seat(status, spanSize))
+                        break
+                    }
+
+                    j++
+                }
+                if (j == statusList.size) {
+                    seatList.add(Seat(status, spanSize))
+                }
+                i += spanSize
+            }
+
+            return seatList
+        }
+    }
+
+    private class SeatAdapter(data: List<Status>) : RxBaseQuickAdapter<Status, BaseViewHolder>(R.layout.item_seat_selection_icon, data) {
         override fun convert(helper: BaseViewHolder, item: Status) {
         }
 
     }
 
-    private class BehindAdapter(data: List<Seat>) : RxBaseQuickAdapter<Seat, BaseViewHolder>(R.layout.item_seat_selection_span, data) {
+    private class SpanAdapter(data: List<Seat>) : RxBaseQuickAdapter<Seat, BaseViewHolder>(R.layout.item_seat_selection_span, data) {
+        init {
+            setSpanSizeLookup { _, i -> mData[i].spanSize }
+        }
+
         override fun convert(helper: BaseViewHolder, seat: Seat) {
             val res = when (seat.status) {
                 Status.IDLE -> R.drawable.selection_default_background
@@ -98,32 +140,5 @@ class SeatSelectionActivity : AppCompatActivity() {
             helper.itemView.setBackgroundResource(res)
         }
 
-    }
-
-    private fun updatePresentationList() {
-        mBehindList.clear()
-        var spanSize: Int
-        var j: Int
-        var i = 0
-        while (i < mFrontList.size) {
-            spanSize = 1
-            val status = mFrontList[i]
-            j = i + 1
-            while (j < mFrontList.size) {
-                if (mFrontList[j] == status) {
-                    spanSize += 1
-                } else {
-                    mBehindList.add(Seat(status, spanSize))
-                    break
-                }
-
-                j++
-            }
-            if (j == mFrontList.size) {
-                mBehindList.add(Seat(status, spanSize))
-            }
-            i += spanSize
-        }
-        mBehindAdapter.notifyDataSetChanged()
     }
 }
