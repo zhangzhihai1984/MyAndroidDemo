6b34a6fbb0e8eee1b9eeae73b2dee125e25a85ff
zhangzhihai <13504984520@139.com>
2020-04-24 14:24:50 +0800

    seat selection view 2

---
+++ app/src/main/java/com/usher/demo/awesome/selection/SeatSelection2Activity.kt
@@ -0,0 +1,76 @@
+package com.usher.demo.awesome.selection
+
+import android.animation.ValueAnimator
+import android.os.Bundle
+import android.view.animation.OvershootInterpolator
+import com.twigcodes.ui.SeatSelectionView
+import com.twigcodes.ui.util.RxUtil
+import com.usher.demo.R
+import com.usher.demo.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_seat_selection2.*
+
+class SeatSelection2Activity : BaseActivity(Theme.DARK_ONLY) {
+    companion object {
+        private const val ROW_COUNT = 8
+        private const val COLUMN_COUNT = 16
+    }
+
+    private lateinit var mSeatData: ArrayList<ArrayList<SeatSelectionView.Status>>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seat_selection)
+        initData()
+        initView()
+    }
+
+    private fun initData() {
+        val mockServerData = Array(ROW_COUNT) {
+            Array(COLUMN_COUNT) {
+                when ((0..6).random()) {
+                    in 0..3 -> "unsold"
+                    4, 5 -> "sold"
+                    else -> "disabled"
+                }
+            }.toList()
+        }.toList()
+
+        /*
+        * 1. 把mockServerData[i][j]数据由服务器定义的字段转为客户端定义的枚举
+        * 2. 把mockServerData内部的List转为ArrayList
+        * 3. 把mockServerData这个List转为ArrayList
+        */
+        mSeatData = ArrayList(
+                mockServerData.map { row ->
+                    row.map { column ->
+                        when (column) {
+                            "unsold" -> SeatSelectionView.Status.IDLE
+                            "sold" -> SeatSelectionView.Status.SELECTED
+                            else -> SeatSelectionView.Status.DISABLED
+                        }
+                    }
+                }.map { row -> ArrayList(row) }
+        )
+    }
+
+    private fun initView() {
+        val countAnimator = ValueAnimator.ofFloat(1f, 1.5f, 1f).apply {
+            duration = 1000
+            interpolator = OvershootInterpolator()
+            addUpdateListener {
+                count_textview.scaleX = animatedValue as Float
+                count_textview.scaleY = animatedValue as Float
+            }
+        }
+
+        seatselectionview.dataChanges()
+                .`as`(RxUtil.autoDispose(this))
+                .subscribe {
+                    val count = mSeatData.flatten().filter { status -> status == SeatSelectionView.Status.PENDING }.size
+                    count_textview.text = "$count"
+                    countAnimator.start()
+                }
+
+        seatselectionview.setData(mSeatData, ROW_COUNT, COLUMN_COUNT)
+    }
+}
--- app/src/main/java/com/usher/demo/main/DemoConfig.kt
+++ app/src/main/java/com/usher/demo/main/DemoConfig.kt
@@ -3,6 +3,7 @@ package com.usher.demo.main
 import com.usher.demo.awesome.channel.ChannelActivity
 import com.usher.demo.awesome.decoration.DecorationActivity
 import com.usher.demo.awesome.drag.DragActiity
+import com.usher.demo.awesome.selection.SeatSelection2Activity
 import com.usher.demo.awesome.selection.SeatSelectionActivity
 import com.usher.demo.awesome.smarthome.SmartHomeActivity
 import com.usher.demo.image.ImageBlurActivity
@@ -94,6 +95,7 @@ object DemoConfig {
 
         configMap[KEY_AWESOME] = listOf(
                 DemoItem("seat selection", SeatSelectionActivity::class.java),
+                DemoItem("seat selection2", SeatSelection2Activity::class.java),
                 DemoItem("edit channel", ChannelActivity::class.java),
                 DemoItem("recyclerView decoration", DecorationActivity::class.java),
                 DemoItem("smart home", SmartHomeActivity::class.java),
