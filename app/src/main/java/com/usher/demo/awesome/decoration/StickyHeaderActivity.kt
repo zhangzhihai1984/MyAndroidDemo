b4c7b98b7e7233cddc7012d805e26e9f8b063b4b
zhangzhihai <13504984520@139.com>
2020-04-29 15:55:23 +0800

    sticky header

--- app/src/main/java/com/usher/demo/awesome/decoration/StickyHeaderActivity.kt
+++ app/src/main/java/com/usher/demo/awesome/decoration/StickyHeaderActivity.kt
@@ -1,10 +1,7 @@
 package com.usher.demo.awesome.decoration
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.Rect
-import android.graphics.RectF
+import android.graphics.*
 import android.os.Bundle
 import android.view.View
 import androidx.core.graphics.drawable.toBitmap
@@ -34,42 +31,57 @@ class StickyHeaderActivity : BaseActivity(Theme.DARK_ONLY) {
                 listOf(*resources.getStringArray(R.array.sticky_list5)),
                 listOf(*resources.getStringArray(R.array.sticky_list6)),
                 listOf(*resources.getStringArray(R.array.sticky_list7))
-        ).mapIndexed { i, list ->
-            list.map { content -> ItemInfo("$i", "GROUP $i", content) }
+        )
+
+        val decorationData = data.mapIndexed { i, list ->
+            list.map { i to "Header $i" }
         }.flatten()
 
         recyclerview.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
-        recyclerview.addItemDecoration(StickyHeaderDecoration(this, data))
-        recyclerview.adapter = StickyHeaderAdapter(data)
+        recyclerview.adapter = StickyHeaderAdapter(data.flatten())
+        recyclerview.addItemDecoration(StickyHeaderDecoration(this, decorationData))
     }
 
-    private class StickyHeaderAdapter(data: List<ItemInfo>) : RxBaseQuickAdapter<ItemInfo, BaseViewHolder>(R.layout.item_sticky_header, data) {
-        override fun convert(helper: BaseViewHolder, item: ItemInfo) {
-            helper.setText(R.id.content_textview, item.content)
+    private class StickyHeaderAdapter(data: List<String>) : RxBaseQuickAdapter<String, BaseViewHolder>(R.layout.item_sticky_header, data) {
+        override fun convert(helper: BaseViewHolder, content: String) {
+            helper.setText(R.id.content_textview, content)
         }
     }
 
-    private class StickyHeaderDecoration(private val context: Context, private val data: List<ItemInfo>) : RecyclerView.ItemDecoration() {
+    private class StickyHeaderDecoration(private val context: Context, private val data: List<Pair<Int, String>>) : RecyclerView.ItemDecoration() {
         companion object {
             private const val DIVIDER_HEIGHT = 3 * 3
-            private const val HEADER_HEIGHT = 40 * 3
+            private const val HEADER_HEIGHT = 30 * 3
             private const val OFFSET_LEFT = 45 * 3
             private const val SIDE_LENGTH = 30 * 3
             private const val CIRCLE_RADIUS = 15 * 3f
         }
 
+        private val mStrokePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+            color = context.getColor(R.color.colorPrimary)
+            style = Paint.Style.STROKE
+            strokeWidth = 6f
+        }
+
+        private val mHeaderPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+            color = context.getColor(R.color.colorPrimary)
+            style = Paint.Style.FILL
+        }
+
+        private val mTextPaint = Paint(Paint.ANTI_ALIAS_FLAG or Paint.FAKE_BOLD_TEXT_FLAG).apply {
+            color = Color.WHITE
+            textAlign = Paint.Align.CENTER
+            textSize = 40f
+        }
+
         override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
-            outRect.top = DIVIDER_HEIGHT.takeIf { parent.getChildAdapterPosition(view) > 0 } ?: 0
+//            outRect.top = DIVIDER_HEIGHT.takeIf { parent.getChildAdapterPosition(view) > 0 } ?: 0
+            outRect.top = HEADER_HEIGHT.takeIf { isFirstViewInGroup(parent.getChildAdapterPosition(view)) }
+                    ?: DIVIDER_HEIGHT
             outRect.left = OFFSET_LEFT
         }
 
         override fun onDraw(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
-            val strokePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-                color = context.getColor(R.color.colorPrimary)
-                style = Paint.Style.STROKE
-                strokeWidth = 6f
-            }
-
             parent.forEach { view ->
                 val centerX = (parent.paddingStart + OFFSET_LEFT / 2).toFloat()
                 val centerY = (view.top + view.height / 2).toFloat()
@@ -82,14 +94,40 @@ class StickyHeaderActivity : BaseActivity(Theme.DARK_ONLY) {
                 dst.right = centerX + SIDE_LENGTH / 2
 
                 c.drawBitmap(bitmap, null, dst, null)
-                c.drawCircle(centerX, centerY, CIRCLE_RADIUS, strokePaint)
-                c.drawLine(centerX, centerY - CIRCLE_RADIUS, centerX, view.top.toFloat() - DIVIDER_HEIGHT, strokePaint)
-                c.drawLine(centerX, centerY + CIRCLE_RADIUS, centerX, view.bottom.toFloat(), strokePaint)
-
+                c.drawCircle(centerX, centerY, CIRCLE_RADIUS, mStrokePaint)
+                c.drawLine(centerX, centerY - CIRCLE_RADIUS, centerX, view.top.toFloat() /*- DIVIDER_HEIGHT*/, mStrokePaint)
+                c.drawLine(centerX, centerY + CIRCLE_RADIUS, centerX, view.bottom.toFloat(), mStrokePaint)
             }
         }
 
         override fun onDrawOver(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+            parent.forEach { view ->
+
+                if (isFirstViewInGroup(parent.getChildAdapterPosition(view))) {
+                    val top = view.top - HEADER_HEIGHT
+                    val bottom = view.top
+                    val left = parent.paddingStart
+                    val right = parent.width - parent.paddingEnd
+
+                    val rect = Rect(left, top, right, bottom)
+
+                    drawHeader(c, rect, parent.getChildAdapterPosition(view))
+                }
+            }
         }
+
+        private fun drawHeader(c: Canvas, rect: Rect, position: Int) {
+            c.drawRect(rect, mHeaderPaint)
+
+            val metrics = mTextPaint.fontMetrics
+            c.drawText(data[position].second, rect.exactCenterX(), rect.exactCenterY() - metrics.top / 2 - metrics.bottom / 2, mTextPaint)
+        }
+
+        private fun isFirstViewInGroup(position: Int) =
+                position == 0 || data[position].first != data[position - 1].first
+
+        private fun isLastViewInGroup(position: Int) =
+                position == data.size - 1 || data[position].first != data[position + 1].first
+
     }
 }
