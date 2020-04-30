a792ea038736dac7255760be338c98e45fbc2800
zhangzhihai <13504984520@139.com>
2020-04-30 14:26:23 +0800

    index view

--- TwigCodesUI/src/main/java/com/twigcodes/ui/IndexView.kt
+++ TwigCodesUI/src/main/java/com/twigcodes/ui/IndexView.kt
@@ -1,22 +1,25 @@
 package com.twigcodes.ui
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
-import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import androidx.lifecycle.LifecycleOwner
 import com.jakewharton.rxbinding3.view.globalLayouts
 import com.twigcodes.ui.util.RxUtil
+import kotlin.math.floor
+import kotlin.math.max
+import kotlin.math.min
 
 class IndexView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0, defStyleRes: Int = 0) : View(context, attrs, defStyleAttr, defStyleRes) {
     companion object {
-        private const val DEFAULT_TEXT_SIZE = 40f
-        private const val DEFAULT_IDLE_COLOR = Color.BLACK
-        private const val DEFAULT_SELECTED_COLOR = Color.RED
+        private const val DEFAULT_TEXT_SIZE = 40
+        private const val DEFAULT_IDLE_COLOR = Color.GRAY
+        private const val DEFAULT_INDEXED_COLOR = Color.BLACK
     }
 
     private val mTextPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
@@ -28,19 +31,25 @@ class IndexView @JvmOverloads constructor(context: Context, attrs: AttributeSet?
         strokeWidth = 3f
     }
 
+    private var mIdleColor: Int
+    private var mIndexedColor: Int
+    private var mTextSize: Float
+
     private val mData = arrayOf("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
             "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "#")
 
     private var mItemHeight = 0f
+    private var mCurrentPosition = -1
 
     init {
-//        val textSize = DEFAULT_TEXT_SIZE
-        val textIdleColor = DEFAULT_IDLE_COLOR
-        val textSelectedColor = DEFAULT_SELECTED_COLOR
+        val a = context.theme.obtainStyledAttributes(attrs, R.styleable.IndexView, defStyleAttr, defStyleRes)
+        mIdleColor = a.getColor(R.styleable.IndexView_idleColor, DEFAULT_IDLE_COLOR)
+        mIndexedColor = a.getColor(R.styleable.IndexView_indexedColor, DEFAULT_INDEXED_COLOR)
+        mTextSize = a.getDimensionPixelSize(R.styleable.IndexView_textSize, DEFAULT_TEXT_SIZE).toFloat()
+        a.recycle()
 
         mTextPaint.run {
-            textSize = DEFAULT_TEXT_SIZE
-            color = DEFAULT_IDLE_COLOR
+            textSize = mTextSize
         }
 
         initView()
@@ -51,13 +60,21 @@ class IndexView @JvmOverloads constructor(context: Context, attrs: AttributeSet?
                 .take(1)
                 .`as`(RxUtil.autoDispose(context as LifecycleOwner))
                 .subscribe {
-                    Log.i("zzh", "width: $width height:$height itemHeight:${height.toFloat() / mData.size}")
                     mItemHeight = height.toFloat() / mData.size
                 }
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(event: MotionEvent): Boolean {
-        return super.onTouchEvent(event)
+        when (event.action) {
+            MotionEvent.ACTION_DOWN, MotionEvent.ACTION_MOVE -> {
+                mCurrentPosition = min(max(floor((event.y / mItemHeight)).toInt(), 0), mData.size - 1)
+                invalidate()
+            }
+            MotionEvent.ACTION_UP -> {
+            }
+        }
+        return true
     }
 
     override fun onDraw(canvas: Canvas) {
@@ -67,7 +84,9 @@ class IndexView @JvmOverloads constructor(context: Context, attrs: AttributeSet?
             canvas.drawLine(0f, i * mItemHeight, width.toFloat(), i * mItemHeight, mDividerPaint)
 
             val metrics = mTextPaint.fontMetrics
-            canvas.drawText(text, width.toFloat() / 2, (i + 0.5f) * mItemHeight - metrics.top / 2 - metrics.bottom / 2, mTextPaint)
+            canvas.drawText(text, width.toFloat() / 2, (i + 0.5f) * mItemHeight - metrics.top / 2 - metrics.bottom / 2, mTextPaint.apply {
+                color = if (i == mCurrentPosition) mIndexedColor else mIdleColor
+            })
         }
     }
 }
