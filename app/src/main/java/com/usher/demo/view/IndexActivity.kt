205cfaa7a13f7ebac7c878e1c0f3d1e04b02af44
zhangzhihai <13504984520@139.com>
2020-05-06 16:04:43 +0800

    index view

--- app/src/main/java/com/usher/demo/view/IndexActivity.kt
+++ app/src/main/java/com/usher/demo/view/IndexActivity.kt
@@ -58,8 +58,8 @@ class IndexActivity : BaseActivity(Theme.LIGHT_AUTO) {
          */
 
         /**
-         * 当滑动IndexView引发索引发生变化时:
-         * 1. 找到属于该组的数据, 进而找到将组内第一个数据对应的索引值, 然后将对应的item滑动至顶部, 由于该item是组内的
+         * 当滑动IndexView导致索引发生变化时:
+         * 1. 找到属于该组的数据, 进而找到将组内第一个数据对应的索引, 然后将对应的item滑动至顶部, 由于该item是组内的
          * 第一个item, 因此该组的header自然会出现在顶部.
          * 2. 将indicator的margin top调整至IndexView对应索引所在的位置, 该值为:
          * indexview_item高度*i + indexview_item高度/2 - indicator高度/2 + indexview.top
@@ -73,7 +73,7 @@ class IndexActivity : BaseActivity(Theme.LIGHT_AUTO) {
          * 将索引重新指向"B", 这时indexChanges两次接收的索引均为"B", 但是索引确实发生了B->A->B的变化)
          *
          * 2. 当滑动IndexView时, 会出现对应的索引所在的组没有数据(比如说联系人中没有以U或V开头的数据)或是所在组的数据
-         * 不足导致该组header无法固定在顶部(比如联系人中以Z开头的数据只有两三个, 那么固定在顶部的header其实是X组的).
+         * 不足导致该组header无法固定在顶部(比如联系人中以Z开头的数据只有两三个, 那么固定在顶部的header其实是Y组的).
          * 但是这不妨碍indicator"对准"IndexView对应索引的位置.
          */
         indexview.indexChanges()
@@ -89,6 +89,15 @@ class IndexActivity : BaseActivity(Theme.LIGHT_AUTO) {
                     indicator_textview.text = content
                 }
 
+        /**
+         * 当滑动RecyclerView时:
+         * 1. 调用IndexView的changeIndex
+         * 2. 将indicator的margin top调整至IndexView对应索引所在的位置(参见对IndexView.indexChanges的处理)
+         *
+         * 注意:
+         * 在滑动IndexView的过程中如果索引发生变化, 会调用[LinearLayoutManager.scrollToPositionWithOffset],
+         * 也就是说RecyclerView会滑动, 但这是"被动"滑动, 与"主动"滑动的区别在于, "被动"滑动的dy为0.
+         */
         recyclerview.scrollEvents()
                 .filter { it.dy != 0 }
                 .filter { recyclerview.isNotEmpty() }
@@ -103,7 +112,20 @@ class IndexActivity : BaseActivity(Theme.LIGHT_AUTO) {
                     indicator_textview.text = content
                 }
 
+        /**
+         * 当滑动IndexView时, 会出现对应的索引所在的组的数据不足导致该组header无法固定在顶部(比如联系人中以Z开头的数据
+         * 只有两三个, 那么固定在顶部的header其实是Y组的), 这时候需要一个"纠错"的操作.
+         *
+         * 过程如下:
+         * 1. 索引变化: 滑动IndexView至"Z"时, 触发indexChanges.
+         * 2. RecyclerView滑动: 将"Z"组第一个数据对应的item滑动至recyclerview的顶部, 但由于数据不足, 固定在顶部的
+         * header其实是Y组的, 顶部item的数据也是Y组的.
+         * 3. Decoration反馈: RecyclerView的滑动触发了[RecyclerView.ItemDecoration.onDrawOver],
+         * Decoration将该事件emit出来.
+         * 4. 索引纠错: RecyclerView顶部item数据所在的组为"Y", 将该索引重新赋给IndexView
+         */
         stickyHeaderDecoration.scrolls()
+                .filter { recyclerview.isNotEmpty() }
                 .compose(RxUtil.getSchedulerComposer())
                 .`as`(RxUtil.autoDispose(this))
                 .subscribe {
@@ -173,10 +195,6 @@ class IndexActivity : BaseActivity(Theme.LIGHT_AUTO) {
         }
 
         /**
-         * 我们首先需要做的是遍历[RecyclerView]的item view, 需要注意的是, [RecyclerView]的item view是复用的,
-         * 所以我们得到的索引值是可见item view的索引值, 如果要获取该item view所对应的"真正的"索引值的话, 需要调用
-         * [RecyclerView.getChildAdapterPosition]获取.
-         *
          * 我们对header的绘制从普遍到特殊分三步进行处理:
          * 1. 在[getItemOffsets]中我们已经在每组item顶部将header的位置预留出来, 首先需要做的
          * 就是把header绘制在给他们预留的应地方. 此时看到的效果就是所有的header和item的滑动是同步的.
@@ -187,8 +205,8 @@ class IndexActivity : BaseActivity(Theme.LIGHT_AUTO) {
          * 一个item的bottom重合时, 也就是下一组的header已经"顶到了"固定在顶部的header时, 那么该header
          * 便不再固定在顶部, 而是随着最后一个item一起滑走.
          *
-         * 需要注意的是, [RecyclerView]的item view是复用的, 所以我们进行遍历得到的索引值是可见item view的索引值,
-         * 如果要获取该item view所对应的"真正的"索引值的话, 需要调用[RecyclerView.getChildAdapterPosition]获取.
+         * 需要注意的是, [RecyclerView]的item view是复用的, 所以我们进行遍历得到的索引是可见item view的索引,
+         * 如果要获取该item view所对应的"真正的"索引的话, 需要调用[RecyclerView.getChildAdapterPosition]获取.
          */
         override fun onDrawOver(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
             mScrollSubject.onNext(Unit)
