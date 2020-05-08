b4c8b6ccddbe8e2cefbd18480b73b35cfb979188
zhangzhihai <13504984520@139.com>
2020-05-08 18:03:17 +0800

    index view

--- app/src/main/java/com/usher/demo/view/IndexActivity.kt
+++ app/src/main/java/com/usher/demo/view/IndexActivity.kt
@@ -95,7 +95,6 @@ class IndexActivity : BaseActivity(Theme.LIGHT_AUTO) {
         /**
          * 滑动IndexView处理
          *
-         * 位置处理:
          * 1. 根据IndexView当前的index, 找到组内首个数据的position, 然后将对应位置的item滑动至顶部, 由于该item
          * 是组内的首个item, 因此该组的header自然会出现在顶部.
          * 2. 将indicator对准IndexView当前index对应位置的item. 调整后的margin top值为:
@@ -105,21 +104,6 @@ class IndexActivity : BaseActivity(Theme.LIGHT_AUTO) {
          * 1. 当滑动IndexView时, 会出现对应的索引所在组没有数据(比如说联系人中没有以U或V开头的数据)或是所在组及之后
          * 所有组的数据不足一屏(比如联系人中以Z开头的数据只有两三个, 那么固定在顶部的header其实是Y组的, Z组的header
          * 无法固定在顶部), 但是这不妨碍indicator"对准"IndexView对应索引的位置.
-         * (1) 如果所在组没有数据: position为-1, [LinearLayoutManager.scrollToPositionWithOffset]中对
-         * position的要求是"starting at 0", 尽管不会看到任何滑动发生, 但是我们依然要调用该方法, 因为这样会触发
-         * [RecyclerView.requestLayout], 进而触发[RecyclerView.ItemDecoration.onDrawOver], 这样我们才有
-         * 机会对IndexView当前的index进行"纠错".
-         * (2) 如果所在组数据不足无法滑动到顶端: 与上面类似, 虽然RecyclerView发生了滑动, 但是没有滑动到预期的位置,
-         * 我们依然需要对IndexView当前的index进行"纠错".
-         *
-         * 动画处理:
-         * 1. MOVE: 不处理, 也就是说让indicator一直显示.
-         * 2. UP: 开始indicator"消失"动画.
-         * 3. DOWN: 正常情况下开始indicator"显示"动画. 但是要考虑一种情况, 就是刚刚经历了UP, 此时的"消失"动画刚刚
-         * 开始, 也就是说indicator并没有消失, 此时如果开始indicator"显示"动画, 那么会让已经出现的indicator重新
-         * 再出现一遍, 如果频繁的进行索引切换操作的话, 用户体验就变得非常糟糕. 在这种情况下我们需要做的是, 结束"消失"
-         * 动画, 让indicator继续显示. 考虑到[ValueAnimator.end]会让indicator的scale直接变为动画结尾的状态,
-         * 也就是0, 因此我们需要将indicator的scale调整至1.
          */
         indexview.touches()
                 .compose(RxUtil.getSchedulerComposer())
@@ -135,6 +119,16 @@ class IndexActivity : BaseActivity(Theme.LIGHT_AUTO) {
                     indicator_textview.text = value
                 }
 
+        /**
+         * 滑动IndexView处理
+         *
+         * 1. 如果indicator未显示, 显示indicator.
+         * 2. 如果超过500ms未滑动:
+         * (1) 隐藏indicator.
+         * (2) 修正IndexView当前的index. 当滑动IndexView时, 会出现对应的索引所在组没有数据(比如说联系人中没有以U或
+         * V开头的数据)或是所在组及之后所有组的数据不足一屏(比如联系人中以Z开头的数据只有两三个, 那么固定在顶部的header
+         * 其实是Y组的, Z组的header无法固定在顶部), 这时需要对IndexView当前的index进行"修正".
+         */
         indexview.touches()
                 .doOnNext {
                     if (indicator_textview.visibility != View.VISIBLE)
@@ -176,6 +170,12 @@ class IndexActivity : BaseActivity(Theme.LIGHT_AUTO) {
                     indicator_textview.text = value
                 }
 
+        /**
+         * 滑动RecyclerView处理
+         *
+         * 1. 如果indicator未显示, 显示indicator.
+         * 2. 如果超过500ms未滑动, 隐藏indicator.
+         */
         recyclerview.scrollEvents()
                 .filter { it.dy != 0 }
                 .filter { recyclerview.isNotEmpty() }
@@ -187,23 +187,6 @@ class IndexActivity : BaseActivity(Theme.LIGHT_AUTO) {
                 .observeOn(AndroidSchedulers.mainThread())
                 .`as`(RxUtil.autoDispose(this))
                 .subscribe { hideAnimator.start() }
-
-        /**
-         * "纠错"处理
-         *
-         * 当滑动IndexView时, 会出现对应的索引所在组及之后所有组的数据不足一屏(比如联系人中以Z开头的数据只有两三个,
-         * 那么固定在顶部的header其实是Y组的, Z组的header无法固定在顶部), 这时需要对IndexView当前的index进行"纠错".
-         *
-         * 过程如下:
-         * 1. 滑动IndexView: 滑动IndexView至"Z"时, IndexView当前index变为"Z".
-         * 2. RecyclerView滑动: 处理IndexView.touches, 调用[LinearLayoutManager.scrollToPositionWithOffset].
-         * 3. Decoration反馈: RecyclerView的滑动触发了[RecyclerView.requestLayout], 进而触发
-         * [RecyclerView.ItemDecoration.onDrawOver], Decoration将该事件emit出来.
-         * 4. 索引纠错: RecyclerView顶部item数据所在的组为"Y", 将该索引重新赋给IndexView.
-         *
-         * 注意: 虽然是"纠错", 一般来说只有在数据"缺失"的情况下才真正需要该操作, 即便IndexView当前的index没有错,
-         * 更新一次也不会有任何副作用. Just in case.
-         */
     }
 
     private class StickyHeaderAdapter(data: List<String>) : RxBaseQuickAdapter<String, BaseViewHolder>(R.layout.item_index, data) {
