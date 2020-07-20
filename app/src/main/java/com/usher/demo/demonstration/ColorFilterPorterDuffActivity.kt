9b7359a2e7853f0cc4b360ecf721771b399aa788
zhangzhihai <13504984520@139.com>
2020-07-20 15:29:14 +0800

    porterduff colorfilter

--- app/src/main/java/com/usher/demo/demonstration/ColorFilterPorterDuffActivity.kt
+++ app/src/main/java/com/usher/demo/demonstration/ColorFilterPorterDuffActivity.kt
@@ -11,10 +11,13 @@ import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager.widget.PagerAdapter
 import com.chad.library.adapter.base.BaseViewHolder
+import com.jakewharton.rxbinding4.viewpager.pageSelections
 import com.twigcodes.ui.adapter.RxBaseQuickAdapter
 import com.twigcodes.ui.fragment.BasePagerFragment
 import com.twigcodes.ui.util.ImageUtil
+import com.twigcodes.ui.util.RxUtil
 import com.usher.demo.R
 import com.usher.demo.base.BaseActivity
 import com.usher.demo.utils.Constants
@@ -29,31 +32,57 @@ class ColorFilterPorterDuffActivity : BaseActivity(Theme.LIGHT_AUTO) {
     }
 
     private fun initView() {
-        val resIds = listOf(R.drawable.demo_hardworking, R.drawable.demo_trump)
+        val resIds = listOf(R.drawable.demo_hardworking, R.drawable.demo_trump, R.drawable.demo_mall)
+        val adapter = ColorFilterFragmentAdapter(supportFragmentManager, resIds)
 
-        viewpager.adapter = ColorFilterFragmentAdapter(supportFragmentManager, resIds)
+        viewpager.adapter = adapter
+        viewpager.pageSelections()
+                .to(RxUtil.autoDispose(this))
+                .subscribe { position ->
+                    original_imageview.setImageResource(resIds[position])
+                }
+
         indicatorview.setViewPager(viewpager)
+
+        color_picker_view.colorPicks()
+                .compose(RxUtil.getSchedulerComposer())
+                .to(RxUtil.autoDispose(this))
+                .subscribe { color ->
+                    adapter.updateColor(color)
+                }
     }
 
-    private class ColorFilterFragmentAdapter(fm: FragmentManager, private val mResIds: List<Int>) : FragmentStatePagerAdapter(fm) {
-        override fun getItem(position: Int): Fragment = ColorFilterFragment.newInstance(mResIds[position])
+    private class ColorFilterFragmentAdapter(fm: FragmentManager, private val mResIds: List<Int>, private var color: Int = Color.RED) : FragmentStatePagerAdapter(fm) {
+        override fun getItem(position: Int): Fragment {
+            return ColorFilterFragment.newInstance(mResIds[position], color)
+        }
 
         override fun getCount(): Int = mResIds.size
+
+        override fun getItemPosition(obj: Any): Int {
+            return PagerAdapter.POSITION_NONE
+        }
+
+        fun updateColor(color: Int) {
+            this.color = color
+            notifyDataSetChanged()
+        }
     }
 
     class ColorFilterFragment : BasePagerFragment(R.layout.fragment_colorfilter_porterduff) {
         companion object {
-            fun newInstance(resId: Int) =
+            fun newInstance(resId: Int, color: Int) =
                     ColorFilterFragment().apply {
                         arguments = Bundle().apply {
-                            putInt(Constants.TAG_DATA, resId)
+                            putIntegerArrayList(Constants.TAG_DATA, arrayListOf(resId, color))
                         }
                     }
         }
 
         override fun init() {
-            arguments?.run {
-                val resId = getInt(Constants.TAG_DATA)
+            arguments?.getIntegerArrayList(Constants.TAG_DATA)?.run {
+                val resId = this[0]
+                val color = this[1]
                 val bitmap = resources.getDrawable(resId, null).toBitmap()
                 val modes = listOf(
                         PorterDuff.Mode.DARKEN,
@@ -64,15 +93,17 @@ class ColorFilterPorterDuffActivity : BaseActivity(Theme.LIGHT_AUTO) {
                         PorterDuff.Mode.OVERLAY
                 )
 
-                recyclerview.layoutManager = GridLayoutManager(requireContext(), 2, RecyclerView.VERTICAL, false)
-                recyclerview.adapter = ColorFilterAdapter(modes, bitmap)
+                val adapter = ColorFilterAdapter(modes, bitmap, color)
+
+                recyclerview.layoutManager = GridLayoutManager(requireContext(), 3, RecyclerView.VERTICAL, false)
+                recyclerview.adapter = adapter
             }
         }
     }
 
-    private class ColorFilterAdapter(data: List<PorterDuff.Mode>, val bitmap: Bitmap) : RxBaseQuickAdapter<PorterDuff.Mode, BaseViewHolder>(R.layout.item_colorfilter_porterduff, data) {
+    private class ColorFilterAdapter(data: List<PorterDuff.Mode>, val bitmap: Bitmap, var color: Int) : RxBaseQuickAdapter<PorterDuff.Mode, BaseViewHolder>(R.layout.item_colorfilter_porterduff, data) {
         override fun convert(helper: BaseViewHolder, mode: PorterDuff.Mode) {
-            helper.getView<ImageView>(R.id.colorfilter_imageview).setImageBitmap(ImageUtil.getColorFilterBitmap(bitmap, Color.RED, mode))
+            helper.getView<ImageView>(R.id.colorfilter_imageview).setImageBitmap(ImageUtil.getColorFilterBitmap(bitmap, color, mode))
             helper.setText(R.id.mode_textview, mode.name)
         }
     }
