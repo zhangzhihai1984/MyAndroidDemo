9619729a875b6d4137a6d3c59fb6e6d54da76e6a
zhangzhihai <13504984520@139.com>
2020-07-25 16:45:32 +0800

    bitmap blur

--- app/src/main/java/com/usher/demo/demonstration/ImageBlurActivity.kt
+++ app/src/main/java/com/usher/demo/demonstration/ImageBlurActivity.kt
@@ -1,25 +1,22 @@
 package com.usher.demo.demonstration
 
 import android.os.Bundle
+import androidx.core.graphics.drawable.toBitmap
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager.widget.PagerAdapter
 import com.jakewharton.rxbinding4.viewpager.pageSelections
-import com.squareup.picasso.Picasso
+import com.jakewharton.rxbinding4.widget.changes
 import com.twigcodes.ui.fragment.BasePagerFragment
 import com.twigcodes.ui.util.ImageUtil
 import com.twigcodes.ui.util.RxUtil
 import com.usher.demo.R
 import com.usher.demo.base.BaseActivity
-import com.usher.demo.utils.Constants
 import kotlinx.android.synthetic.main.activity_image_blur.*
 import kotlinx.android.synthetic.main.fragment_image_blur.*
 
 class ImageBlurActivity : BaseActivity(Theme.LIGHT_AUTO) {
-    companion object {
-        private val RES_IDS = listOf(R.drawable.duggee1, R.drawable.duggee2, R.drawable.duggee3, R.drawable.duggee4, R.drawable.duggee5)
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_image_blur)
@@ -27,35 +24,68 @@ class ImageBlurActivity : BaseActivity(Theme.LIGHT_AUTO) {
     }
 
     private fun initView() {
-        viewpager.adapter = BlurFragmentAdapter(supportFragmentManager, RES_IDS)
+        val resIds = listOf(R.drawable.demo_hardworking, R.drawable.demo_tree, R.drawable.demo_arale)
+        val adapter = BlurFragmentAdapter(supportFragmentManager, resIds, 8f, 8f)
+
+        viewpager.adapter = adapter
         viewpager.pageSelections()
                 .to(RxUtil.autoDispose(this))
                 .subscribe { position ->
-                    Picasso.get().load(RES_IDS[position]).transform(ImageUtil.getBlurTransformation(this)).into(blur_imageview)
+                    original_imageview.setImageResource(resIds[position])
+                }
+//
+        indicatorview.setViewPager(viewpager)
+
+        scale_seekbar.changes()
+                .to(RxUtil.autoDispose(this))
+                .subscribe { scale ->
+                    adapter.updateBlur(scale.toFloat(), radius_seekbar.progress.toFloat())
                 }
 
-        indicatorview.setViewPager(viewpager)
+        radius_seekbar.changes()
+                .to(RxUtil.autoDispose(this))
+                .subscribe { radius ->
+                    adapter.updateBlur(scale_seekbar.progress.toFloat(), radius.toFloat())
+                }
     }
 
-    private class BlurFragmentAdapter(fm: FragmentManager, private var mData: List<Int>) : FragmentStatePagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
-        override fun getItem(position: Int): Fragment = BlurFragment.newInstance(mData[position])
+    private class BlurFragmentAdapter(fm: FragmentManager, private var mResIds: List<Int>, private var scale: Float, private var radius: Float) : FragmentStatePagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+        override fun getItem(position: Int): Fragment = BlurFragment.newInstance(mResIds[position], scale, radius)
 
-        override fun getCount(): Int = mData.size
+        override fun getCount(): Int = mResIds.size
+
+        override fun getItemPosition(obj: Any): Int = PagerAdapter.POSITION_NONE
+
+        fun updateBlur(scale: Float, radius: Float) {
+            this.scale = scale
+            this.radius = radius
+            notifyDataSetChanged()
+        }
     }
 
     class BlurFragment : BasePagerFragment(R.layout.fragment_image_blur) {
         companion object {
-            fun newInstance(resId: Int) =
+            private const val RESID = "RESID"
+            private const val SCALE = "SCALE"
+            private const val RADIUS = "RADIUS"
+
+            fun newInstance(resId: Int, scale: Float, radius: Float) =
                     BlurFragment().apply {
                         arguments = Bundle().apply {
-                            putInt(Constants.TAG_DATA, resId)
+                            putInt(RESID, resId)
+                            putFloat(SCALE, scale)
+                            putFloat(RADIUS, radius)
                         }
                     }
         }
 
         override fun init() {
             arguments?.run {
-                Picasso.get().load(getInt(Constants.TAG_DATA)).into(pager_imageview)
+                val resId = getInt(RESID)
+                val scale = getFloat(SCALE)
+                val radius = getFloat(RADIUS)
+                val bitmap = resources.getDrawable(resId, null).toBitmap()
+                blur_imageview.setImageBitmap(ImageUtil.getRenderScriptBlurScaledBitmap(requireContext(), bitmap, scale, radius))
             }
         }
     }
