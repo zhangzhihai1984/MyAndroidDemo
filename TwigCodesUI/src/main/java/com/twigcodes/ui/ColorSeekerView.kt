8f3ad630e80684e4e10eca2eb9dcef027de15ba3
zhangzhihai <13504984520@139.com>
2020-07-27 13:01:53 +0800

    color seeker

--- TwigCodesUI/src/main/java/com/twigcodes/ui/ColorSeekerView.kt
+++ TwigCodesUI/src/main/java/com/twigcodes/ui/ColorSeekerView.kt
@@ -1,13 +1,50 @@
 package com.twigcodes.ui
 
 import android.content.Context
+import android.graphics.Color
 import android.util.AttributeSet
 import android.widget.LinearLayout
+import androidx.lifecycle.LifecycleOwner
+import com.jakewharton.rxbinding4.view.globalLayouts
+import com.jakewharton.rxbinding4.widget.changes
+import com.twigcodes.ui.util.RxUtil
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.subjects.PublishSubject
+import kotlinx.android.synthetic.main.color_seeker_layout.view.*
 
 class ColorSeekerView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0, defStyleRes: Int = 0) : LinearLayout(context, attrs, defStyleAttr, defStyleRes) {
+    private val mColorSeekSubject = PublishSubject.create<Int>()
 
     init {
         inflate(context, R.layout.color_seeker_layout, this)
         orientation = VERTICAL
+
+        initView()
     }
+
+    private fun initView() {
+        globalLayouts()
+                .take(1)
+                .switchMap { Observable.merge(red_seekbar.changes(), green_seekbar.changes(), blue_seekbar.changes()) }
+                .to(RxUtil.autoDispose(context as LifecycleOwner))
+                .subscribe { updateColor() }
+    }
+
+    fun updateColor(color: Int) {
+        val red = Color.red(color)
+        val green = Color.green(color)
+        val blue = Color.blue(color)
+
+        updateColor(red, green, blue)
+    }
+
+    fun updateColor(red: Int, green: Int, blue: Int) {
+        red_seekbar.progress = red
+        green_seekbar.progress = green
+        blue_seekbar.progress = blue
+    }
+
+    private fun updateColor() = mColorSeekSubject.onNext(Color.argb(255, red_seekbar.progress, green_seekbar.progress, blue_seekbar.progress))
+
+    fun colorSeeks(): Observable<Int> = mColorSeekSubject
 }
--- app/src/main/java/com/usher/demo/view/ColorSeekerActivity.kt
+++ app/src/main/java/com/usher/demo/view/ColorSeekerActivity.kt
@@ -1,12 +1,26 @@
 package com.usher.demo.view
 
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
 import android.os.Bundle
+import com.twigcodes.ui.util.RxUtil
 import com.usher.demo.R
 import com.usher.demo.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_color_seeker.*
 
 class ColorSeekerActivity : BaseActivity(Theme.LIGHT_AUTO) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_color_seeker)
+        initView()
+    }
+
+    private fun initView() {
+        color_seeker_view.colorSeeks()
+                .compose(RxUtil.getSchedulerComposer())
+                .to(RxUtil.autoDispose(this))
+                .subscribe { color ->
+                    palette_imageview.colorFilter = PorterDuffColorFilter(color, PorterDuff.Mode.SRC_IN)
+                }
     }
 }
