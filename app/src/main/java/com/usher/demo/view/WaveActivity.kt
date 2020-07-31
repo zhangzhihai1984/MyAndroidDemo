b3333f72c7d02819705c6d1fdfa2a53ee2260d42
zhangzhihai <13504984520@139.com>
2020-07-31 13:29:31 +0800

    optimize wave demo

--- app/src/main/java/com/usher/demo/view/WaveActivity.kt
+++ app/src/main/java/com/usher/demo/view/WaveActivity.kt
@@ -1,12 +1,11 @@
 package com.usher.demo.view
 
+import android.animation.ValueAnimator
 import android.os.Bundle
-import com.twigcodes.ui.util.RxUtil
+import android.view.animation.DecelerateInterpolator
 import com.usher.demo.R
 import com.usher.demo.base.BaseActivity
-import io.reactivex.rxjava3.core.Observable
 import kotlinx.android.synthetic.main.activity_wave.*
-import java.util.concurrent.TimeUnit
 
 class WaveActivity : BaseActivity(Theme.LIGHT_AUTO) {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,14 +15,23 @@ class WaveActivity : BaseActivity(Theme.LIGHT_AUTO) {
     }
 
     private fun initView() {
-        Observable.interval(0, 200, TimeUnit.MILLISECONDS)
-                .take(100 + 1)
-                .map { it.toInt() }
-                .compose(RxUtil.getSchedulerComposer())
-                .to(RxUtil.autoDispose(this))
-                .subscribe {
-                    wave_view.progress = it
-                    progressbar.progress = it
-                }
+//        Observable.interval(0, 200, TimeUnit.MILLISECONDS)
+//                .take(100 + 1)
+//                .map { it.toInt() }
+//                .compose(RxUtil.getSchedulerComposer())
+//                .to(RxUtil.autoDispose(this))
+//                .subscribe {
+//                    wave_view.progress = it
+//                    progressbar.progress = it
+//                }
+
+        ValueAnimator.ofInt(0, 100).apply {
+            duration = 10_000
+            interpolator = DecelerateInterpolator()
+            addUpdateListener {
+                wave_view.progress = animatedValue as Int
+                progressbar.progress = animatedValue as Int
+            }
+        }.start()
     }
 }
