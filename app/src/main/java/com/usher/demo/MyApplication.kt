750e40dbb3815020ecccc847f4da96b3a00c4567
zhangzhihai <13504984520@139.com>
2020-10-10 14:57:54 +0800

    UPDATE GRADLE (Manifest & Application) for 4.4

---
+++ app/src/main/java/com/usher/demo/MyApplication.kt
@@ -0,0 +1,13 @@
+package com.usher.demo
+
+import android.app.Application
+import android.content.Context
+import androidx.multidex.MultiDex
+
+class MyApplication : Application() {
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+    }
+}
