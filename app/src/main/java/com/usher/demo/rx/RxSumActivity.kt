68290efa6912a901b7348d35b8c350fba6b6f5a1
zhangzhihai <13504984520@139.com>
2021-09-03 15:46:49 +0800

    rx sum

--- app/src/main/java/com/usher/demo/rx/RxSumActivity.kt
+++ app/src/main/java/com/usher/demo/rx/RxSumActivity.kt
@@ -4,7 +4,7 @@ import com.jakewharton.rxbinding4.view.clicks
 import com.twigcodes.ui.util.RxUtil
 import com.usher.demo.R
 import com.usher.demo.base.BaseActivity
-import io.reactivex.rxjava3.kotlin.Observables
+import io.reactivex.rxjava3.core.Observable
 import kotlinx.android.synthetic.main.activity_rx_sum.*
 
 class RxSumActivity : BaseActivity(R.layout.activity_rx_sum, Theme.LIGHT_AUTO) {
@@ -12,16 +12,16 @@ class RxSumActivity : BaseActivity(R.layout.activity_rx_sum, Theme.LIGHT_AUTO) {
     override fun initView() {
         val param1 = param1_textview.clicks()
                 .map { 1 }
-                .scan { acc, curr -> acc + curr }
-                .doOnNext { param1_textview.text = "$it" }
+                .scan { acc, value -> acc + value }
+                .doOnNext { param1_textview.text = it.toString() }
 
         val param2 = param2_textview.clicks()
                 .map { 1 }
-                .scan { t1, t2 -> t1 + t2 }
-                .doOnNext { param2_textview.text = "$it" }
+                .scan { acc, value -> acc + value }
+                .doOnNext { param2_textview.text = it.toString() }
 
-        Observables.combineLatest(param1, param2) { t1, t2 -> t1 + t2 }
+        Observable.combineLatest(param1, param2) { t1, t2 -> t1 + t2 }
                 .to(RxUtil.autoDispose(this))
-                .subscribe { sum_textview.text = "$it" }
+                .subscribe { sum_textview.text = it.toString() }
     }
 }
