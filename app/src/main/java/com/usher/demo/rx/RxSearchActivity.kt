d22c075a88f0935d388fd41e5e2f6bf41fed0994
zhangzhihai <13504984520@139.com>
2021-09-09 17:29:39 +0800

    rx search

--- app/src/main/java/com/usher/demo/rx/RxSearchActivity.kt
+++ app/src/main/java/com/usher/demo/rx/RxSearchActivity.kt
@@ -8,35 +8,40 @@ import com.twigcodes.ui.adapter.RxBaseQuickAdapter
 import com.twigcodes.ui.util.RxUtil
 import com.usher.demo.R
 import com.usher.demo.base.BaseActivity
+import io.reactivex.rxjava3.core.Observable
 import kotlinx.android.synthetic.main.activity_rx_search.*
 import kotlinx.android.synthetic.main.item_search.view.*
 import java.util.concurrent.TimeUnit
 
 class RxSearchActivity : BaseActivity(R.layout.activity_rx_search, Theme.LIGHT_AUTO) {
+    private val mCities = listOf("北京市", "上海市", "天津市", "重庆市", "沈阳市", "台北市")
+    private val mAdapter by lazy { SearchAdapter(listOf()) }
+
+    private val getCities: (keyword: String) -> Observable<List<String>> = { keyword ->
+        Observable.create { emitter ->
+            val cities = mCities.filter { city -> city.contains(keyword) }
+            emitter.onNext(cities)
+        }
+    }
 
     override fun initView() {
-        val searchResults = arrayListOf<String>()
-        val cities = listOf("北京市", "上海市", "天津市", "重庆市", "沈阳市", "台北市")
-        val adapter = SearchAdapter(searchResults)
-
         recyclerview.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
-        recyclerview.adapter = adapter
+        recyclerview.adapter = mAdapter
 
         edittext.textChanges()
                 .debounce(500, TimeUnit.MILLISECONDS)
-                .map { it.trim() }
+                .map { it.trim().toString() }
                 .distinctUntilChanged()
-                .map { cities.filter { city -> city.contains(it) } }
+                .switchMap { getCities(it) }
                 .compose(RxUtil.getSchedulerComposer())
                 .to(RxUtil.autoDispose(this))
                 .subscribe {
-                    searchResults.clear()
-                    searchResults.addAll(it)
-                    adapter.notifyDataSetChanged()
+                    mAdapter.setNewData(it)
                 }
     }
 
     private class SearchAdapter(data: List<String>) : RxBaseQuickAdapter<String, BaseViewHolder>(R.layout.item_search, data) {
+
         override fun convert(holder: BaseViewHolder, item: String) {
             holder.itemView.name_textview.text = item
         }
