4de7562b4cd7f5a4ee0287a4aece25085d655e12
zhangzhihai <13504984520@139.com>
2021-09-19 17:23:58 +0800

    night mode

--- app/src/main/java/com/usher/demo/setting/NightModeActivity.kt
+++ app/src/main/java/com/usher/demo/setting/NightModeActivity.kt
@@ -1,11 +1,14 @@
 package com.usher.demo.setting
 
+import android.content.Intent
 import com.jakewharton.rxbinding4.view.clicks
 import com.twigcodes.ui.util.RxUtil
 import com.usher.demo.R
 import com.usher.demo.base.BaseActivity
+import com.usher.demo.main.DemoListActivity
 import com.usher.demo.utils.CommonUtil
 import com.usher.demo.widget.CommonDialog
+import io.reactivex.rxjava3.core.Observable
 import kotlinx.android.synthetic.main.activity_night_mode.*
 
 class NightModeActivity : BaseActivity(R.layout.activity_night_mode) {
@@ -16,13 +19,25 @@ class NightModeActivity : BaseActivity(R.layout.activity_night_mode) {
         title_view.endTextClicks()
                 .compose(RxUtil.singleClick())
                 .flatMap {
-                    CommonDialog(this)
-                            .withContent(R.string.night_mode_save_hint)
-                            .withDialogType(CommonDialog.ButtonType.DOUBLE_PRIMARY)
-                            .clicks()
+                    if (mPendingNightModeStrategy != mCurrentNightModeStrategy)
+                        CommonDialog(this)
+                                .withContent(R.string.night_mode_save_hint)
+                                .withDialogType(CommonDialog.ButtonType.DOUBLE_PRIMARY)
+                                .clicks()
+                                .map { true }
+                    else
+                        Observable.just(false)
                 }
                 .to(RxUtil.autoDispose(this))
-                .subscribe { }
+                .subscribe { restart ->
+                    CommonUtil.setNightModeStrategy(this, mPendingNightModeStrategy)
+                    if (restart) {
+                        startActivity(Intent(this, DemoListActivity::class.java).apply {
+                            flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                        })
+                    }
+                    finish()
+                }
 
         night_only_layout.clicks()
                 .to(RxUtil.autoDispose(this))
