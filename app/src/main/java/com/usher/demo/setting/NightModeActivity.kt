0c1e935a0de0a96c8b7e27b14ca651206cbf2b5c
zhangzhihai <13504984520@139.com>
2021-09-19 12:47:40 +0800

    night mode

---
+++ app/src/main/java/com/usher/demo/setting/NightModeActivity.kt
@@ -0,0 +1,10 @@
+package com.usher.demo.setting
+
+import com.usher.demo.R
+import com.usher.demo.base.BaseActivity
+
+class NightModeActivity: BaseActivity(R.layout.activity_night_mode) {
+
+    override fun initView() {
+    }
+}
--- app/src/main/java/com/usher/demo/setting/SettingActivity.kt
+++ app/src/main/java/com/usher/demo/setting/SettingActivity.kt
@@ -1,10 +1,18 @@
 package com.usher.demo.setting
 
+import android.content.Intent
+import com.jakewharton.rxbinding4.view.clicks
+import com.twigcodes.ui.util.RxUtil
 import com.usher.demo.R
 import com.usher.demo.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_setting.*
 
 class SettingActivity : BaseActivity(R.layout.activity_setting) {
 
     override fun initView() {
+        night_mode_layout.clicks()
+                .compose(RxUtil.singleClick())
+                .to(RxUtil.autoDispose(this))
+                .subscribe { startActivity(Intent(this, NightModeActivity::class.java)) }
     }
 }
