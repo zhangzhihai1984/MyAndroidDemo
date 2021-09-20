07dabf97f1b9277bb298652d265e73d3dba24818
zhangzhihai <13504984520@139.com>
2021-09-20 13:42:47 +0800

    night mode

--- app/src/main/java/com/usher/demo/setting/SettingActivity.kt
+++ app/src/main/java/com/usher/demo/setting/SettingActivity.kt
@@ -5,11 +5,18 @@ import com.jakewharton.rxbinding4.view.clicks
 import com.twigcodes.ui.util.RxUtil
 import com.usher.demo.R
 import com.usher.demo.base.BaseActivity
+import com.usher.demo.utils.CommonUtil
 import kotlinx.android.synthetic.main.activity_setting.*
 
 class SettingActivity : BaseActivity(R.layout.activity_setting) {
 
     override fun initView() {
+        night_mode_textview.text = when (CommonUtil.getNightModeStrategy(this)) {
+            NightModeStrategy.AUTO -> getString(R.string.setting_night_mode_auto)
+            NightModeStrategy.YES -> getString(R.string.setting_night_mode_yes)
+            NightModeStrategy.NO -> getString(R.string.setting_night_mode_no)
+        }
+
         night_mode_layout.clicks()
                 .compose(RxUtil.singleClick())
                 .to(RxUtil.autoDispose(this))
